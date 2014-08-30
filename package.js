2521269e97b22fd1c22d5bb395d3f0a3f4d1bf8e
Sivli Kestanous <cmtoups@me.com>
2014-08-29 21:47:33 -0400

    Meteor 0.9.0 support
    Patched up for Meteor 0.9.0 support. This should do it but obviously I have not tested it.

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: "Simple package to use gravatar images"
+  summary: "Simple package to use gravatar images",
+  version: "0.0.5",
+  git: "https://github.com/tmeasday/meteor-gravatar"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use('crypto-md5', ['client', 'server']);
   if(api.export)
     api.export('Gravatar');
